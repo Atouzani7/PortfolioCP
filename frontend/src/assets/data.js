5abc03792604cc97dae473bb7f70e23cc4b6bc86
Asma <asma.touzani7@gmail.com>
2023-02-01 15:46:22 +0100

    adding data + reset.css

--- frontend/src/assets/data.js
+++ frontend/src/assets/data.js
@@ -1,98 +1,106 @@
 {
-  "Techno": [
-    {
-      "id": 1,
-      "name": "HTML",
-      "gestion": "front"
+  "techno"[
+    ({
+      id: 1,
+      name: "HTML",
+      gestion: "front",
     },
     {
-      "id": 2,
-      "name": "CSS",
-      "gestion": "front"
+      id: 2,
+      name: "CSS",
+      gestion: "front",
     },
     {
-      "id": 3,
-      "name": "Javascript",
-      "gestion": "front"
+      id: 3,
+      name: "Javascript",
+      gestion: "front",
     },
     {
-      "id": 4,
-      "name": "React.js",
-      "gestion": "front"
+      id: 4,
+      name: "React.js",
+      gestion: "front",
     },
     {
-      "id": 5,
-      "name": "Design UI / UX",
-      "gestion": "front"
+      id: 5,
+      name: "Design UI / UX",
+      gestion: "front",
     },
     {
-      "id": 6,
-      "name": "Figma",
-      "gestion": "Front"
+      id: 6,
+      name: "Figma",
+      gestion: "Front",
     },
     {
-      "id": 7,
-      "name": "Git",
-      "gestion": "front - back"
+      id: 7,
+      name: "Git",
+      gestion: "front - back",
     },
     {
-      "id": 8,
-      "name": "MySQL",
-      "gestion": "Back"
+      id: 8,
+      name: "MySQL",
+      gestion: "Back",
     },
     {
-      "id": 9,
-      "name": "Node.js",
-      "gestion": "Back"
+      id: 9,
+      name: "Node.js",
+      gestion: "Back",
     },
     {
-      "id": 10,
-      "name": "Express.js",
-      "gestion": "Back"
-    }
+      id: 10,
+      name: "Express.js",
+      gestion: "Back",
+    })
   ],
-  "Projet": [
-    {
-      "id": 1,
-      "name": "La Ruche",
-      "description": "Premier projet lors de la formation, Trombinoscope de notre promo de développeurs.",
-      "lien": "https://github.com/Atouzani7/P1-laRuche.git",
-      "image": "https://files.slack.com/files-pri/T6SG2QGG2-F04N8AWBCDN/logo_p1.png"
-    },
-    {
-      "id": 2,
-      "name": "Blind Test",
-      "description": "Jeu musical. L’objectif est de trouver le plus vite possible le titre de la chanson joué.",
-      "lien": "https://github.com/WildCodeSchool/2022-09-JS-Lille-project-2-g1-techlegends.git",
-      "image": "https://files.slack.com/files-pri/T6SG2QGG2-F04C0QW2SHY/logo-tech-legend.png"
-    },
-    {
-      "id": 3,
-      "name": "Siloc",
-      "description": "",
-      "lien": "https://github.com/Sax593/Siloc.git",
-      "image": ""
-    },
-    {
-      "id": 4,
-      "name": "Origin's Digital",
-      "description": "Origins Digital est une plateforme où vous pouvez regarder vos films / séries  favoris",
-      "lien": "https://github.com/WildCodeSchool/2022-09-JS-Lille-project-3-g3-origin.git",
-      "image": "https://files.slack.com/files-pri/T6SG2QGG2-F04JBCDN1FZ/logo.png"
-    }
-  ],
-  "Entreprise": [
-    {
-      "id": 1,
-      "name": "Wild Code School",
-      "description": "La Wild Code School est une école innovante et un réseau européen de campus qui forment aux métiers tech des spécialistes adaptables. C'est une marque de la société Innov'Educ.",
-      "lien": "https://www.wildcodeschool.com/en-GB"
-    },
-    {
-      "id": 2,
-      "name": "Origin's Digital",
-      "description": "Netco Sports est une entreprise française spécialisée dans la conception et le développement d'applications second écran et de services OTT dans l'industrie du Sport.",
-      "lien": "https://www.origins-digital.com"
-    }
-  ]
+    "Projet"[
+      ({
+        id: 1,
+        name: "La Ruche",
+        description:
+          "Premier projet lors de la formation, Trombinoscope de notre promo de développeurs.",
+        lien: "https://github.com/Atouzani7/P1-laRuche.git",
+        image:
+          "https://files.slack.com/files-pri/T6SG2QGG2-F04N8AWBCDN/logo_p1.png",
+      },
+      {
+        id: 2,
+        name: "Blind Test",
+        description:
+          "Jeu musical. L’objectif est de trouver le plus vite possible le titre de la chanson joué.",
+        lien: "https://github.com/WildCodeSchool/2022-09-JS-Lille-project-2-g1-techlegends.git",
+        image:
+          "https://files.slack.com/files-pri/T6SG2QGG2-F04C0QW2SHY/logo-tech-legend.png",
+      },
+      {
+        id: 3,
+        name: "Siloc",
+        description: "",
+        lien: "https://github.com/Sax593/Siloc.git",
+        image: "",
+      },
+      {
+        id: 4,
+        name: "Origin's Digital",
+        description:
+          "Origins Digital est une plateforme où vous pouvez regarder vos films / séries  favoris",
+        lien: "https://github.com/WildCodeSchool/2022-09-JS-Lille-project-3-g3-origin.git",
+        image:
+          "https://files.slack.com/files-pri/T6SG2QGG2-F04JBCDN1FZ/logo.png",
+      })
+    ],
+    "Entreprise"[
+      ({
+        id: 1,
+        name: "Wild Code School",
+        description:
+          "La Wild Code School est une école innovante et un réseau européen de campus qui forment aux métiers tech des spécialistes adaptables. C'est une marque de la société Innov'Educ.",
+        lien: "https://www.wildcodeschool.com/en-GB",
+      },
+      {
+        id: 2,
+        name: "Origin's Digital",
+        description:
+          "Netco Sports est une entreprise française spécialisée dans la conception et le développement d'applications second écran et de services OTT dans l'industrie du Sport.",
+        lien: "https://www.origins-digital.com",
+      })
+    ];
 }
