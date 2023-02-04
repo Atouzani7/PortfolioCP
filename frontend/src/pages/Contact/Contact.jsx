ae512f5a5611ab52ec945dccdacff7d149647fab
Asma <asma.touzani7@gmail.com>
2023-02-04 23:20:34 +0100

    style textearea

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -2,26 +2,30 @@ import "./contact.scss";
 import Send from "@assets/send.svg";
 
 export default function Contact() {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+  };
+
   return (
     <div className="ContactPage">
-      <form className="formContact">
+      <form className="formContact" onChange={handleSubmit}>
         <label>
           Nom
-          <input type="texte" placeholder="Nom" />
+          <input type="texte" placeholder="Nom" onChange={handleSubmit} />
         </label>
         <label>
           E-mail
-          <input type="texte" placeholder="E-mail" />
+          <input onChange={handleSubmit} type="texte" placeholder="E-mail" />
         </label>
         <label>
           Sujet
-          <input type="texte" placeholder="Sujet" />
+          <input onChange={handleSubmit} type="texte" placeholder="Sujet" />
         </label>
-        <label>
+        <label className="message">
           Message
-          <input type="texte" className="message" />
+          <textarea />
         </label>
-        <button type="submit" className="submit">
+        <button onChange={handleSubmit} type="submit" className="submit">
           Envoyer
           <img src={Send} alt="send" className="send" />
         </button>
